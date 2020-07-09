09b12beb120bebe83ff792137cc59534b32315e2
Júlio Lopes <julio.lopes@outsystems.com>
2020-07-09 23:49:08 +0100

    fix: remove lib folder

--- packages/label/lib/label.js
+++
@@ -1,2 +0,0 @@
-function e(e){return e&&"object"==typeof e&&"default"in e?e.default:e}var n=e(require("react"));function t(){var e,n,r=(e=["\n  color: #333;\n  font-size: 1rem;\n  font-weight: 600;\n"],n||(n=e.slice(0)),e.raw=n,e);return t=function(){return r},r}var r=e(require("styled-components")).span(t());module.exports=function(e){return n.createElement(r,null,e.text)};
-//# sourceMappingURL=label.js.map
